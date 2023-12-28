ab2ebd59287d820ab03761b4eb4363d162ae3e11
igorpieruccini <igorpieruccini@gmail.com>
2023-12-28 17:23:22 +0100

    Update git-log.example

--- src/git-log.js
+++ src/git-log.js
@@ -1,13 +1,13 @@
 const gitLog = [
   "commit 0c7df6cc365d701a9a5629589831b4d2997ee585\nAuthor: Mr. example <example@myemail.com>\nDate:   Fri Dec 15 09:05:42 2023 +0100\n\n    v2.17.8\n\n",
-  "commit 8c6c8804d904e8d0919dc6eeb3a2e845f367d266\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 14 17:03:39 2023 +0100\n\n    NO-TICKET Update styling for active certificate button, keep styling f… (#321)\n\n",
+  "commit 8c6c8804d904e8d0919dc6eeb3a2e845f367d266\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 14 17:03:39 2023 +0100\n\n    NO-TICKET Update styling for active certificate button\n\n",
   "commit 805c9ad116e3ae58c528de7eb3862c4c758c0f9f\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 14 16:19:00 2023 +0100\n\n    NO-TICKET Providing flex on correct parent (#319)\n\n",
   "commit 6ad0bd7e835d057f9e0d8ce5dea143e9de5a8c8a\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 14 15:07:44 2023 +0100\n\n    NO-TICKET Fix demo certificate issue (#318)\n\n",
   "commit 74a5a253bebb7d423081e896e285c7c04f04256c\nAuthor: Mr. example <example@myemail.com>\nDate:   Wed Dec 13 16:43:54 2023 +0100\n\n    NO-TICKET Fix firefox carousel bug (#315)\n\n",
   "commit f5a3680772b6c3595203adb01ea09231f7d33fc9\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 14 15:24:25 2023 +0100\n\n    v2.17.7\n\n",
-  "commit 74357db634eb24840edb6b59702668495a14ad62\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 7 09:43:47 2023 +0000\n\n     NO-TICKET Fix header logo width\n    \n    Co-authored-by: transifex-integration[bot] <43880903+transifex-integration[bot]@users.noreply.github.com>\n    Co-authored-by: sosafe-olha-melnykovych <114214981+sosafe-olha-melnykovych@users.noreply.github.com>\n\n",
-  "commit 7eb0f71b73330525991fe5c4cf5d078a84f5b8b9\nAuthor: Mr. example <example@myemail.com>\nDate:   Wed Dec 6 15:45:32 2023 +0100\n\n     NO-TICKET Module due date (#303)\n    \n    Co-authored-by: igorpieruccini <igorpieruccini@gmail.com>\n\n",
-  "commit 773d31bffdf93f21a781f93b72a007288f2ee891\nAuthor: Mr. example <example@myemail.com>\nDate:   Wed Nov 29 15:25:57 2023 +0000\n\n    Updates for project Elearning (#285)\n    \n    Co-authored-by: transifex-integration[bot] <43880903+transifex-integration[bot]@users.noreply.github.com>\n\n",
+  "commit 74357db634eb24840edb6b59702668495a14ad62\nAuthor: Mr. example <example@myemail.com>\nDate:   Thu Dec 7 09:43:47 2023 +0000\n\n     NO-TICKET Fix header logo width\n\n",
+  "commit 7eb0f71b73330525991fe5c4cf5d078a84f5b8b9\nAuthor: Mr. example <example@myemail.com>\nDate:   Wed Dec 6 15:45:32 2023 +0100\n\n     NO-TICKET Module due date (#303)\n\n",
+  "commit 773d31bffdf93f21a781f93b72a007288f2ee891\nAuthor: Mr. example <example@myemail.com>\nDate:   Wed Nov 29 15:25:57 2023 +0000\n\n    Updates for project Elearning (#285)\n\n",
   "commit 53d8913d10593df291f7e4a94e3bcbd2ee5e1448\nAuthor: Mr. example <example@myemail.com>\nDate:   Tue Dec 12 11:04:11 2023 +0100\n\n    v2.17.6\n\n",
   "commit dfbd33954d5e6c1d763360c3bc3c0ef7494fe2d4\nAuthor: Mr. example <example@myemail.com>\nDate:   Tue Dec 12 11:00:30 2023 +0100\n\n    NO-TICKET Remove survey hub from classic (#312)\n    \n    Co-authored-by: igorpieruccini <igorpieruccini@gmail.com>\n"]
 window["gitLog"] = gitLog;
