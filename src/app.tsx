1a5c169307f88532b333a688c84832b832888741
igorpieruccini <igorpieruccini@gmail.com>
2023-12-30 22:42:02 +0100

    Add antd theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
-import { Empty, Flex } from 'antd';
+import { ConfigProvider, Empty, Flex } from 'antd';
 import React from 'react';
 import { CommitContextProvider } from './commit-context/commit-context';
 import { CommitList } from './commit-list/commit-list';
 import { TextEditor } from './text-editor/text-editor';
+import { theme } from './theme';
 
 export function App() {
   if (!window["gitLog"]) {
     return <Empty />
   }
-  return <CommitContextProvider commitsSTR={window["gitLog"]}>
-    <Flex gap={12} >
-      <CommitList />
-      <TextEditor />
-    </Flex>
-  </ CommitContextProvider>
+  return <ConfigProvider theme={theme}>
+    <CommitContextProvider commitsSTR={window["gitLog"]}>
+      <Flex gap={12} >
+        <CommitList />
+        <TextEditor />
+      </Flex>
+    </ CommitContextProvider>
+  </ConfigProvider>
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+import { ThemeConfig } from "antd";
+
+export const theme: ThemeConfig = {
+  components: {
+    Tag: {
+      colorBorder: "#e8daff",
+      defaultBg: "#f6f2ff",
+      defaultColor: "#491d8b",
+      padding: 4,
+      borderRadiusSM: 100,
+    },
+  },
+  token: {
+    colorPrimaryBg: "#f6f2ff",
+    colorPrimary: "#005e69",
+    colorPrimaryBorder: "#e8daff",
+    colorWarning: "#8e6a00",
+    colorWarningBg: "#fcf4d6",
+    colorWarningBorder: "#F1C21C",
+    colorSuccess: "#198038",
+    colorSuccessBg: "#defbe6",
+    colorSuccessBorder: "#6fdc8c",
+    fontFamily: "Manrope, sans-serif",
+    borderRadius: 8,
+  },
+};
